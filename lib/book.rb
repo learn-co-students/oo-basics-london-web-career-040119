62946977771edf69ffdd6a91b9c8daa1630d4cb7
Anthea Merton <anthea.merton@gmail.com>
2019-04-04 10:58:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  attr_accessor :author
+  
+  def page_count=(num)
+    @page_count=num
+  end
+  
+  def page_count
+    @page_count
+  end
+attr_accessor :genre
 
+  
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
 
